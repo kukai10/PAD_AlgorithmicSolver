5cc0559f147a3c97ec24c3f9ebd105bcc55d1f09
kukaiN <kukainakahata@gmail.com>
2020-01-27 14:04:54 -0500

    Update screensize.py

--- screensize.py
+++ screensize.py
@@ -5,7 +5,3 @@ for sc in screen_aspect_ratio:
     for pd in pad_ratio:
         num = int((sc[1]/pd[1])*pd[0]*unit_length)
         print(sc, pd, "x =", num, ", left over = ", sc[0]*unit_length-num, ", half = ", sc[0]*unit_length/2)
-
-print(720/40)
-#2 = 9
-#1   x
